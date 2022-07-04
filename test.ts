c5e69967877e884bb4df246d91b631a49d1d28c2
螺旋 <dupenghui@cai-inc.com>
2022-07-04 14:45:33 +0800

    feat:style

---
+++ test.ts
@@ -0,0 +1,1 @@
+let p={"hostPrompted":["pre.lecaiyun.com","work-staging.lecaiyun.com","work.lecaiyun.com","login.test.lecaiyun.com","www.yuque.com","blog.csdn.net","www.npmjs.com","sso.cai-inc.com","github.com","cn.bing.com","yapi.cai-inc.com","yuque.antfin-inc.com","zoo.cai-inc.com","www.cnblogs.com"]}
