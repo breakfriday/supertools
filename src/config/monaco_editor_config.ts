532b2fd70635c9852bd0788aa2e1b5d05cbd6092
螺旋 <dupenghui@cai-inc.com>
2022-07-05 15:22:47 +0800

    config

--- src/config/monaco_editor_config.ts
+++ src/config/monaco_editor_config.ts
@@ -1,31 +1,30 @@
 import {
-    DEFAULT_DATA,
-    LANGUAGE_JSON,
-    DEFAULT_FONT_FAMILY,
-    SHOW_FOLDING_CONTROLS,
-  } from './constants';
-  
-  export function getEditorConfig(value: string): object {
-    return {
-      value: value || DEFAULT_DATA,
-      language: LANGUAGE_JSON,
-  
-      minimap: {
-        enabled: false,
-      },
-      fontFamily: DEFAULT_FONT_FAMILY,
-      fontSize: 13,
-  
-      contextmenu: true,
-      scrollBeyondLastLine: false,
-      folding: true,
-      showFoldingControls: SHOW_FOLDING_CONTROLS,
-  
-      useTabStops: true,
-      wordBasedSuggestions: true,
-      quickSuggestions: true,
-      suggestOnTriggerCharacters: true,
-    };
-    
-  }
-  
+  DEFAULT_DATA,
+  LANGUAGE_JSON,
+  DEFAULT_FONT_FAMILY,
+  SHOW_FOLDING_CONTROLS,
+} from './constants';
+
+export function getEditorConfig(value: string): object {
+  return {
+    value: value || DEFAULT_DATA,
+    language: LANGUAGE_JSON,
+
+    minimap: {
+      enabled: false,
+    },
+    fontFamily: DEFAULT_FONT_FAMILY,
+    fontSize: 13,
+
+    contextmenu: true,
+    scrollBeyondLastLine: false,
+    folding: true,
+    showFoldingControls: SHOW_FOLDING_CONTROLS,
+
+    useTabStops: true,
+    wordBasedSuggestions: true,
+    quickSuggestions: true,
+    suggestOnTriggerCharacters: true,
+  };
+}
+
