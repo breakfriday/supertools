f6aa830bec6232f1de2219ae39c1bbc8ab6c1ff2
螺旋 <dupenghui@cai-inc.com>
2022-07-05 17:53:28 +0800

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,21 @@
 
+import { POPUP_HTML_PATH } from '@/config/constants';
+
+export function openLink(url: string, isInner = false): void {
+  chrome.tabs.create(
+    { url: isInner ? chrome.extension.getURL(url) : url },
+    (tab) => {
+      // Tab opened.
+    },
+  );
+}
 
 const Home = () => {
-  return  <div>this is the test</div>;
+  return (<div onClick={() => {
+    openLink(POPUP_HTML_PATH, true);
+  }}
+  >this is the test
+  </div>);
 };
 
 export default Home;
