2b0e7b57889be8ba3b1a4e7bec6f88803f942a16
螺旋 <dupenghui@cai-inc.com>
2022-07-06 15:18:15 +0800

    update

--- src/pages/Home/components/service_pannel/index.tsx
+++ src/pages/Home/components/service_pannel/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Table } from '@alifd/next';
+import { Table, Button } from '@alifd/next';
+import styles from './index.module.scss';
 
 function ServicePannel() {
   return (
-    <div>
-      <Table dataSource={[]} hasBorder={false}>
+    <div className={styles.box_pannel}>
+      <div >
+        <Button >添加</Button>
+      </div>
+
+      <Table dataSource={[]} hasBorder={false} className={styles['table_box']} tableLayout="auto">
         <Table.Column title="接口" dataIndex="id" />
         <Table.Column title="类型" dataIndex="time" />
         <Table.Column title="操作" dataIndex="time" />
