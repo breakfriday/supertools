fdc6845735557e962e5751569978c3dc1becf683
螺旋 <dupenghui@cai-inc.com>
2022-07-06 12:14:42 +0800

    feat

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,18 @@ import { runApp, IAppConfig } from 'ice';
 import LocaleProvider from '@/components/LocaleProvider';
 import { getLocale } from '@/utils/locale';
 
+// 脏代码，待mpa
+try {
+  chrome.devtools.panels.create('super_tools', 'images/grey_128.png', 'app.html', (panel) => {
+
+  });
+} catch (e) {
+  console.log(e);
+}
+
+// 创建自定义侧边栏
+
+
 const locale = getLocale();
 
 const appConfig: IAppConfig = {
