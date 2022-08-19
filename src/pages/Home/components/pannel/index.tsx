32f4d1b2c687b9acd70eec57ca18fb2a52f535c3
螺旋 <dupenghui@cai-inc.com>
2022-08-19 17:21:28 +0800

    feat:style

--- src/pages/Home/components/pannel/index.tsx
+++ src/pages/Home/components/pannel/index.tsx
@@ -9,13 +9,13 @@ function Pannel() {
   return (
     <div>
       <Tab>
-        <Tab.Item title="http服务代理 配置" key="1" >
+        <Tab.Item title="服务接口代理" key="1" >
           <ServicePannel />
         </Tab.Item>
-        <Tab.Item title="资源代理 配置" key="2" >
+        <Tab.Item title="前端资源配置" key="2" >
           <AssetsPannel />
         </Tab.Item>
-        <Tab.Item title="模块代理 配置" key="3" >
+        <Tab.Item title="模块组件代理" key="3" >
           <ModulePannel />
         </Tab.Item>
       </Tab>
