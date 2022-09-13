a6a0f5c3cc963d08c031646d47cfb6cb7b885b39
螺旋 <dupenghui@cai-inc.com>
2022-09-13 19:04:39 +0800

    feat:style

---
+++ src/enums.ts
@@ -0,0 +1,21 @@
+export enum UrlType {
+  REG = 'reg',
+  STRING = 'string',
+}
+
+export enum Enabled {
+  YES = 'enabled',
+  NO = 'disabled',
+}
+
+export enum IconBackgroundColor {
+  ON = '#1890ff',
+  OFF = '#bfbfbf',
+  ERROR = '#f5222d',
+}
+
+export enum BadgeText {
+  ERROR = 'Error',
+  OFF = 'OFF',
+  ON = 'ON',
+}
