732c036ae5e8b7315510502eae150b07edee314b
螺旋 <dupenghui@cai-inc.com>
2022-10-10 20:24:41 +0800

    feat:update

--- src/controller/db_service.ts
+++ src/controller/db_service.ts
@@ -74,18 +74,29 @@ class DbService {
   }
 
 
-  addData(storeName, data) {
-    const request = this.db.transaction([storeName], 'readwrite') // 事务对象 指定表格名称和操作模式（"只读"或"读写"）
-      .objectStore(storeName) // 仓库对象
-      .add(data);
+  async addData(storeName, data) {
+    const objectStore = await this._getObjectStore(SCENSE_TABLE_NAME);
 
-    request.onsuccess = function (event) {
-      console.log('数据更新成功');
-    };
+    return new Promise((resolve, reject) => {
+    //   const request = this.db.transaction([storeName], 'readwrite') // 事务对象 指定表格名称和操作模式（"只读"或"读写"）
 
-    request.onerror = function (event) {
-      console.log('数据更新失败');
-    };
+      const request = objectStore.add(data);
+
+      request.onsuccess = function (event) {
+        console.log('数据更新成功');
+        resolve({
+          success: true,
+        });
+      };
+
+      request.onerror = function (event) {
+        console.log('数据更新失败');
+        resolve({
+          success: false,
+          message: '更新失败',
+        });
+      };
+    });
   }
 }
 
